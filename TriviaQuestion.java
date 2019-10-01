b3a40df7628818346b021d574bf751be06b1509f
Arshia Hsn <arshiahsn@gmail.com>
2019-10-01 17:55:49 -0600

    Add files via upload
    Before refactoring

---
+++ TriviaQuestion.java
@@ -0,0 +1,28 @@
+
+
+public class TriviaQuestion
+{
+	public static final int TRUEFALSE = 0;
+	public static final int FREEFORM = 1;
+
+	public String question;		// Actual question
+	public String answer;		// Answer to question
+	public int value;			// Point value of question
+	public int type;			// Question type, TRUEFALSE or FREEFORM
+
+	public TriviaQuestion()
+	{
+		question = "";
+		answer = "";
+		value = 0;
+		type = FREEFORM;
+	}
+
+	public TriviaQuestion(String q, String a, int v, int t)
+	{
+		question = q;
+		answer = a;
+		value = v;
+		type = t;
+	}
+}
